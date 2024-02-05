1476d17dea69da35932de8e7206fdd39dcabfda3
vimpario <koz.viktor2020@gmail.com>
2024-02-05 19:46:22 +0300

    Created repository

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyautogui
+import keyboard
+import time
+
+def auto_click(num_clicks, interval):
+    print("Start autoclicker")
+    while True:
+        keyboard.wait("F6")
+        for _ in range(num_clicks):
+            if keyboard.is_pressed("F7"):
+                print("Pressed F7")
+                return
+            print("Clicked")
+            pyautogui.click()
+            time.sleep(interval)
+
+
+    print("End clicking")
+
+num_clicks = 50
+interval = 1
+
+auto_click(num_clicks, interval)
